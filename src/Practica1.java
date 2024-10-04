baa312cc6c110d6a860c68f774d16ae619a444b8
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-10-04 13:06:37 -0500

    comit 2

---
+++ src/Practica1.java
@@ -0,0 +1,11 @@
+public class Practica1 {
+    public static void main(String[] args) {
+
+        System.out.println("Richard Padilla");
+        System.out.println("ZABALA");
+        System.out.println("0962909642");
+        System.out.println("2004");
+
+
+    }
+}
